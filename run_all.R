1b042461448acc8d1a9410ffa92463abd2a0a2c3
Tadema Ayokunle John <tadeayokunle@gmail.com>
2026-07-30 10:40:41 +0100

    Refactor stages to use R script paths
    Updated stages to include R scripts with correct paths.

--- run_all.R
+++ run_all.R
@@ -14,19 +14,19 @@
 # ============================================================================
 
 stages <- c(
-  "00_setup.R",
-  "01a_data_prep_SPI.R",
-  "01b_rainfall_interpolation_sensitivity.R",
-  "02_spatial_choropleth_moran.R",
-  "03a_spi_wavelet_lag_analysis.R",
-  "03b_spi_wavelet_figures.R",
-  "03c_peak_month_rayleigh_test.R",
-  "04_spi_sarimax_zinb_models.R",
-  "04b_pooled_nb_cluster_interaction.R",
-  "05_spi_rolling_forecast_alerts.R",
-  "05b_spi_alert_sensitivity_PI.R",
-  "06_spi_bivariate_state_synchrony.R",
-  "07_spi_bivariate_synchrony_figures.R"
+  "R/00_setup.R",
+  "R/01a_data_prep_SPI.R",
+  "R/01b_rainfall_interpolation_sensitivity.R",
+  "R/02_spatial_choropleth_moran.R",
+  "R/03a_spi_wavelet_lag_analysis.R",
+  "R/03b_spi_wavelet_figures.R",
+  "R/03c_peak_month_rayleigh_test.R",
+  "R/04_spi_sarimax_zinb_models.R",
+  "R/04b_pooled_nb_cluster_interaction.R",
+  "R/05_spi_rolling_forecast_alerts.R",
+  "R/05b_spi_alert_sensitivity_PI.R",
+  "R/06_spi_bivariate_state_synchrony.R",
+  "R/07_spi_bivariate_synchrony_figures.R"
 )
 
 for (stage in stages) {
